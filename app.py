94449f0a6165eddef19f75de5731e8551ebf136b
Prashanth D <prashanthd1.vc@fkipl.flipkart.com>
2025-06-20 05:30:53 +0530

    Added upload route and static folder

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "static/snapshots"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'No file part', 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+
+    file.save(os.path.join(UPLOAD_FOLDER, 'sheet_snapshot.pdf'))
+    return 'Uploaded', 200
